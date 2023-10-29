e16b4d8536a3339bbf98d4d2b420b12dcbe964b4
fanjx <jack.h.b.fan@outlook.com>
2023-10-29 16:34:13 +0800

    增加过滤文件

--- app/src/main/java/com/example/composek/model/entity/ArticleEntity.kt
+++ app/src/main/java/com/example/composek/model/entity/ArticleEntity.kt
@@ -3,5 +3,5 @@ package com.example.composek.model.entity
 data class ArticleEntity(
     val title: String,
     var source: String,
-    var timestamp: Int
+    var timestamp: String
 )
--- app/src/main/java/com/example/composek/viewmodel/ArticleViewModel.kt
+++ app/src/main/java/com/example/composek/viewmodel/ArticleViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.composek.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.composek.model.entity.ArticleEntity
 
-class ArticleViewModel:ViewModel() {
+class ArticleViewModel : ViewModel() {
 
     // 新闻列表数据
+    var list = listOf(
+        ArticleEntity(title = "这是一条新闻", source = "来源", timestamp = "2023-10-29"),
+        ArticleEntity(title = "这是2条新闻", source = "来源2", timestamp = "2023-10-21"),
+        ArticleEntity(title = "这是3条新闻", source = "来源3", timestamp = "2023-10-12"),
+        ArticleEntity(title = "这是45条新闻", source = "来源4", timestamp = "2023-10-22"),
+        ArticleEntity(title = "这是5条新闻", source = "来源5", timestamp = "2023-10-34"),
+        ArticleEntity(title = "这是6条新闻", source = "来源6", timestamp = "2023-10-29"),
+        ArticleEntity(title = "这是7条新闻", source = "来源7", timestamp = "2023-10-30"),
+    )
 
 }
